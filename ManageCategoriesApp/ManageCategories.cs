a25490fe4684207a6793865586d08966dc2650d6
Trieu Hoang Hieu <hieuthhe180751@fpt.edu.vn>
2024-06-05 23:06:30 +0700

    Add project files.

---
+++ ManageCategoriesApp/ManageCategories.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageCategoriesApp
+{
+    public record Category
+    {
+        public string Name { get; init; }
+        public string CategoryName { get; init; }
+    }
+    public class ManageCategories
+    {
+        SqlConnection connection;
+        SqlCommand command;
+        String ConnectionString = "Server=(local);uid=sa;pwd=123456;Database=MyStore";
+        public List<Category> GetCategories() {
+            List<Category> categories = new List<Category>();
+            connection = new SqlConnection(ConnectionString);
+            string SQL= "Select CategoryID, CategoryName from Categories";
+            command = new SqlCommand(SQL, connection);
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader(CommandBehavior.CloseConnection);
+                if(reader.HasRows == true)
+                {
+                    while(reader.Read())
+                    {
+                        categories.Add(new Category
+                        {
+                            CategoryID = reader.GetInt32("CategoryID"),
+                            CategoryName = reader.GetString("CategoryName")
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return categories;
+        }
+        public void InsertCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+           command = new SqlCommand("Insert into Categories(CategoryName) values(@CategoryName)", connection);
+            command.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+            try
+            {
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        public void UpdateCategory(Category category)
+        {
+            connection = new SqlConnection(ConnectionString);
+            string SQL= "Update Categories set CategoryName = @CategoryName where CategoryID = @CategoryID";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+            command.Parameters.AddWithValue("@CategoryName", category.CategoryName);
+            try
+            {
+   connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+        }
+
+    }
+        public void DeleteCategory(Category category)
+        {
+        connection = new SqlConnection(ConnectionString);
+        string SQL = "Delete from Categories where CategoryID = @CategoryID";
+            command = new SqlCommand(SQL, connection);
+            command.Parameters.AddWithValue("@CategoryID", category.CategoryID);
+            try
+            {
+   connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+
+}
